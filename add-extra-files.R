d63a2afc7c86041ec4be6d3632338e84caba6076
Ethan White <ethan@weecology.org>
2023-03-12 09:41:06 -0400

    Don't keep an extra copy of webr files
    Just get them form the quarto extension

--- add-extra-files.R
+++ add-extra-files.R
@@ -1,6 +1,6 @@
 # Copy the required webr js files into the build directory
-file.copy("webr-serviceworker.js", "_site/webr-serviceworker.js")
-file.copy("webr-worker.js", "_site/webr-worker.js")
+file.copy("_extensions/coatless/webr/webr-serviceworker.js", "_site/webr-serviceworker.js")
+file.copy("_extensions/coatless/webr/webr-worker.js", "_site/webr-worker.js")
 
 # Copy the custom headers file for netlify to improve speed and security
 # See https://docs.r-wasm.org/webr/latest/serving.html
