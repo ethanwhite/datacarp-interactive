0ea0fd9c884a2bcf3de5436b8cb0a28efc7cfe7d
Ethan White <ethan@weecology.org>
2023-03-12 09:11:02 -0400

    Ensure webr files and headers are included in build

---
+++ add-extra-files.R
@@ -0,0 +1,7 @@
+# Copy the required webr js files into the build directory
+file.copy("webr-serviceworker.js", "_site/webr-serviceworker.js")
+file.copy("webr-worker.js", "_site/webr-worker.js")
+
+# Copy the custom headers file for netlify to improve speed and security
+# See https://docs.r-wasm.org/webr/latest/serving.html
+file.copy("_headers", "_site/_headers")
